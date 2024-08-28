2a74af7d1d0e7bdfb170eff9c6f144b77e0fc9dc
KVNG007 <davidarifalo001@gmail.com>
2024-08-28 09:42:55 +0100

    Desktop view completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,21 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { Logo } from './Icons'
 import { ArrowButton } from "./Buttons"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { ChevronDownIcon } from '@radix-ui/react-icons'
 
 const Navbar = () => {
   return (
@@ -17,9 +32,67 @@ const Navbar = () => {
             <li>Home</li>
           </Link>
 
-          <Link href={`#`} className=''>
+          {/* <Link href={`#`} className=''>
             <li>Our products</li>
-          </Link>
+          </Link> */}
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <p className='flex flex-row items-center gap-1' >Our products <ChevronDownIcon fontSize={10} color='grey' /></p>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="font-Roboto w-56">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  Profile
+                  <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  Billing
+                  <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  Settings
+                  <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  Keyboard shortcuts
+                  <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+
+              <DropdownMenuSeparator />
+
+              <DropdownMenuGroup>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuSub>
+                  <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
+                  <DropdownMenuPortal>
+                    <DropdownMenuSubContent className='font-Roboto'>
+                      <DropdownMenuItem>Email</DropdownMenuItem>
+                      <DropdownMenuItem>Message</DropdownMenuItem>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>More...</DropdownMenuItem>
+                    </DropdownMenuSubContent>
+                  </DropdownMenuPortal>
+                </DropdownMenuSub>
+                <DropdownMenuItem>
+                  New Team
+                  <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>GitHub</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuItem disabled>API</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                Log out
+                <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
 
           <Link href={`#`} className=''>
             <li>Consulting</li>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
-const config = {
+const config: Config = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -18,6 +18,32 @@ const config = {
       },
     },
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        grey: '#D6D6D6',
+        accent: '#FD7B03',
+        mild_orange: '#FFF4E0',
+        light_green: '#EBFFF5',
+        rare_green: '#017A591A',
+  
+        secondary: '#1D1D1F',
+        company_red: '#EB7070',
+        brand_yellow: '#D8AE2E',
+        primary: '#F5F5F7',
+        primary_info: '#5094FF'
+      },
+      fontFamily: {
+        Roboto: ["Roboto Mono", "monospace"],
+        Vietnam: ["Be Vietnam Pro", "sans-serif"],
+      },
+      boxShadow: {
+        '4xl': '0px 4px 16px 0px rgba(0, 0, 0, 0.10)',
+        'special': '0px 4px 16px 0px rgba(0, 0, 0, 0.1)',
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -37,4 +63,6 @@ const config = {
   plugins: [require("tailwindcss-animate")],
 } satisfies Config
 
-export default config
+export default config;
+
+
